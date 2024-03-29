762880201dadd5fedf4068cc0ada3130d8f0b024
Jann Louis Ontiveros <louis.ontiveros20@gmail.com>
2024-03-29 22:36:38 +0800

    2nd commit designed shop page

--- UI/src/App.js
+++ UI/src/App.js
@@ -1,11 +1,30 @@
 
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCatergory from './Pages/ShopCatergory';
+import Product from './Pages/Product';
+import LoginSigup from './Pages/LoginSigup';
+import Cart from './Pages/Cart';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCatergory category="men"/>}/>
+        <Route path='/womens' element={<ShopCatergory category="women"/>}/>
+        <Route path='/kids' element={<ShopCatergory category="kid"/>}/>
+        <Route path="/product" element ={<Product/>}>
+          <Route path=':productId' element={<Product/>}/>
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSigup/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ UI/src/Pages/ShopCatergory.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShopCatergory = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ShopCatergory
