b0117e7d9bc820fe220b658cfde412d542abcb81
Jann Louis Ontiveros <louis.ontiveros20@gmail.com>
2024-04-10 01:10:55 +0800

    HomePage Done

--- UI/src/App.js
+++ UI/src/App.js
@@ -7,6 +7,7 @@ import ShopCatergory from './Pages/ShopCatergory';
 import Product from './Pages/Product';
 import LoginSigup from './Pages/LoginSigup';
 import Cart from './Pages/Cart';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/login' element={<LoginSigup/>}/>
       </Routes>
+      <Footer/>
       </BrowserRouter>
     </div>
   );
