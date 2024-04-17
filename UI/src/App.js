949e031ab9044b14a509b51b259ddc64ba38c52d
Jann Louis Ontiveros <louis.ontiveros20@gmail.com>
2024-04-17 21:16:10 +0800

    add some product in the other pages

--- UI/src/App.js
+++ UI/src/App.js
@@ -8,6 +8,9 @@ import Product from './Pages/Product';
 import LoginSigup from './Pages/LoginSigup';
 import Cart from './Pages/Cart';
 import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png';
+import women_banner from './Components/Assets/banner_women.png';
+import kid_banner from './Components/Assets/banner_kids.png'
 
 function App() {
   return (
@@ -16,9 +19,9 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Shop/>}/>
-        <Route path='/mens' element={<ShopCatergory category="men"/>}/>
-        <Route path='/womens' element={<ShopCatergory category="women"/>}/>
-        <Route path='/kids' element={<ShopCatergory category="kid"/>}/>
+        <Route path='/mens' element={<ShopCatergory banner ={men_banner} category="men"/>}/>
+        <Route path='/womens' element={<ShopCatergory banner={women_banner} category="women"/>}/>
+        <Route path='/kids' element={<ShopCatergory banner={kid_banner}category="kid"/>}/>
         <Route path="/product" element ={<Product/>}>
           <Route path=':productId' element={<Product/>}/>
         </Route>
