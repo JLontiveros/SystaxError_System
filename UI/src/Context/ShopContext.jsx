9c0814f72c2089f5dae2682101ad3f52db3da8ed
Jann Louis Ontiveros <louis.ontiveros20@gmail.com>
2024-05-02 11:40:33 +0800

    Frontend done

--- UI/src/Context/ShopContext.jsx
+++ UI/src/Context/ShopContext.jsx
@@ -20,12 +20,38 @@ const ShopContextProvider = (props)=>{
         console.log(cartItems);
     }
 
-    const removefromCart = (itemId) =>{
+    const removeFromCart = (itemId) =>{
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
     }
 
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for(const item in cartItems)
+        {
+            if(cartItems[item]>0)
+            {
+                let itemInfo = all_product.find((product)=>product.id===Number(item))
+                totalAmount += itemInfo.new_price * cartItems[item]
+            }  
+            
+        }
+        return totalAmount;
+        
+    }
 
-    const contextValue = {all_product,cartItems,addToCart,removefromCart};
+    const getTotalCartItems = () =>{
+        let totalItem = 0;
+        for(const item in cartItems)
+        {
+            if(cartItems[item]>0)
+            {
+                totalItem += cartItems[item];
+            }
+        }
+        return totalItem;
+    }
+
+    const contextValue = {getTotalCartItems,getTotalCartAmount,all_product,cartItems,addToCart,removeFromCart};
 
     return(
         <ShopContext.Provider value={contextValue}>
