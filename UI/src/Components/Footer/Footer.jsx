deca39c539a4a8f2eb2f8b23b88ce688e1b593d0
Jann Louis Ontiveros <louis.ontiveros20@gmail.com>
2024-05-11 23:15:15 +0800

    A fully responsive pages

--- UI/src/Components/Footer/Footer.jsx
+++ UI/src/Components/Footer/Footer.jsx
@@ -32,7 +32,7 @@ const Footer = () => {
       </div>
       <div className="footer-copyright">
         <hr/>
-        <p>Copyright @ 2024 - All Right Reserved</p>
+        <p>BSIT 3 A - SYNTAX ERROR</p>
       </div>
     </div>
   )
