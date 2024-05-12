774c591c416258c68ec21a6884dac004ce58f542
Jann Louis Ontiveros <louis.ontiveros20@gmail.com>
2024-05-13 00:46:50 +0800

    resive some images dasd

--- UI/src/Components/Footer/Footer.jsx
+++ UI/src/Components/Footer/Footer.jsx
@@ -10,14 +10,13 @@ const Footer = () => {
     <div className='footer'>
       <div className="footer-logo">
         <img src={footer_logo} alt="" />
-        <p>SHOPPER</p>
+        <p>Bark's n Meow</p>
       </div>
       <ul className="footer-links">
         <li>Company</li>
         <li>Products</li>
         <li>Offices</li>
         <li>About</li>
-        <li>Contact</li>
       </ul>
       <div className="footer-social-icon">
         <div className="footer-icons-container">
--- UI/src/Components/Hero/Hero.jsx
+++ UI/src/Components/Hero/Hero.jsx
@@ -3,22 +3,21 @@ import './Hero.css'
 import hand_icon from '../Assets/hand_icon.png'
 import arrow_icon from '../Assets/arrow.png'
 import hero_image from '../Assets/hero_image.png'
-
 const Hero = () => {
   return (
     <div className='hero'>
       <div className="hero-left">
-        <h2>NEW ARRIVALS ONLY</h2>
+        <h2>"Royal Canin Supported"</h2>
         <div>
         <div className="hero-hand-icon">
                 <p>new</p>
                 <img src={hand_icon} alt="" />
         </div>  
-        <p>collection</p>
-        <p>for everyone</p>
+        <p>Nourishing pets, enriching lives.</p>
+        <p>for cats and dogs</p>
         </div>
         <div className="hero-latest-btn">
-            <div>Latest Collection</div>
+            <div>Royal Canin</div>
             <img src={arrow_icon} alt="" />
         </div>
       </div>
