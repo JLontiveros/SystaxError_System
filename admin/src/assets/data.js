be69aefe009c0e56034b16b43aaab48b286e6887
Jann Louis Ontiveros <louis.ontiveros20@gmail.com>
2024-05-15 03:38:22 +0800

    done with backend

---
+++ admin/src/assets/data.js
@@ -0,0 +1,34 @@
+import p1_img from './product_1.png'
+import p2_img from './product_2.png'
+import p3_img from './product_3.png'
+import p4_img from './product_4.png'
+
+let data_product = [
+  {
+    id:1,
+    name:"Royal Canin Feline Health Nutrition Kitten Wet Cat Food 85g (12 pouches)",
+    image:p1_img,
+    new_price:960.00,
+    old_price:980.00,
+  },
+  {id:2,
+    name:"Royal Canin Feline Health Nutrition Kitten Sterilised 37 Dry Cat Food 2kg",
+    image:p2_img,
+    new_price:1489.00,
+    old_price:1500.00,
+  },
+  {id:3,
+    name:"Royal Canin Feline Health Nutrition Senior Ageing 12+ Dry Cat Food 2kg",
+    image:p3_img,
+    new_price:1500.00,
+    old_price:1600.00,
+  },
+  {id:4,
+    name:"Royal Canin Feline Health Nutrition Senior Instinctive Wet Cat Food 85g (12 pouches)",
+    image:p4_img,
+    new_price:1020.00,
+    old_price:1100.00,
+  },
+];
+
+export default data_product;
---
+++ admin/src/assets/new_collections.js
@@ -0,0 +1,69 @@
+import p1_img from "./product_12.png";
+import p2_img from "./product_35.png";
+import p3_img from "./product_14.png";
+import p4_img from "./product_8.png";
+import p5_img from "./product_15.png";
+import p6_img from "./product_2.png";
+import p7_img from "./product_17.png";
+import p8_img from "./product_28.png";
+
+let new_collections = [
+  {
+    id: 12,
+    name: "Zoi Adult Tuna Dry Cat Food 1kg",
+    image: p1_img,
+    new_price: 139.0,
+    old_price: 150.0,
+  },
+  {
+    id: 35,
+    name: "Kennel Pro Pet Water Fountain",
+    image: p2_img,
+    new_price: 999.0,
+    old_price: 1100.0,
+  },
+  {
+    id: 14,
+    name: "Pedigree Pro Puppy High Protein All Breed Beef and Lamb Dry Dog Food 1.3kg",
+    image: p3_img,
+    new_price: 405.0,
+    old_price: 420.0,
+  },
+  {
+    id: 8,
+    name: "Carnilove Kitten Salmon and Turkey Dry Cat Food 2kg",
+    image: p4_img,
+    new_price: 1079.0,
+    old_price: 1200.,
+  },
+  {
+    id: 15,
+    name: "Pedigree Mother and Baby Milk Dry Dog Food 1.3kg",
+    image: p5_img,
+    new_price: 329.0,
+    old_price: 350.0,
+  },
+  {
+    id: 2,
+    name: "Royal Canin Feline Health Nutrition Kitten Sterilised 37 Dry Cat Food 2kg",
+    image: p6_img,
+    new_price: 1489.00,
+    old_price: 1500.00,
+  },
+  {
+    id: 17,
+    name: "Vitality High Energy Small Breed Dry Dog Food 3kg",
+    image: p7_img,
+    new_price: 869.0,
+    old_price: 900.0,
+  },
+  {
+    id: 28,
+    name: "Approved Cat Condo SJ3 Brown",
+    image: p8_img,
+    new_price: 5624.10,
+    old_price: 6249.0,
+  },
+];
+
+export default new_collections;
